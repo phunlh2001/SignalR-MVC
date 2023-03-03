45b39ecf5b0ed01f945b9376fb2def9cd8cf2c2c
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-03-03 23:08:38 +0700

    feat: seed data init to db

---
+++ Extensions/ModelBuilderExtension.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Test_SignalR.Models;
+
+namespace Test_SignalR.Extensions
+{
+    public static class ModelBuilderExtension
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    ProdID = 1,
+                    ProdName = "Name1",
+                    Category = "Caterory",
+                    StockQty = 100,
+                    UnitPrice = "bag"
+                },
+                new Product
+                {
+                    ProdID = 2,
+                    ProdName = "Name2",
+                    Category = "Caterory",
+                    StockQty = 100,
+                    UnitPrice = "bag"
+                },
+                new Product
+                {
+                    ProdID = 3,
+                    ProdName = "Name3",
+                    Category = "Caterory",
+                    StockQty = 100,
+                    UnitPrice = "bag"
+                }
+            );
+        }
+    }
+}
