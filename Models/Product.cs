26a5e577d1165c1681af059889f8ed36bdd6025e
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-03-03 23:09:10 +0700

    feat: add model

---
+++ Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Test_SignalR.Models
+{
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ProdID { get; set; }
+        public string ProdName { get; set; }
+        public string Category { get; set; }
+        public string UnitPrice { get; set; }
+        public int StockQty { get; set; }
+    }
+}
